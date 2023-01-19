6a89b4501ca3ea829a48222bba382dfa13e2038f
Aidan Williams <aidanw4@gmail.com>
2023-01-19 17:19:01 +0700

    add hamburger menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { FaFlask } from "react-icons/fa";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav id="top" className='nav-container'>
       <a href="#top" className="nav-logo">
         <FaFlask />
         <h1>Mike's STEM Tutoring Services</h1>
       </a>
-      <div className="nav-sections">
-        <a href="#">About Me</a>
-        <a href="#">Services</a>
-        <a href="#">Testimonials</a>
-        <a href="#">Prices</a>
-        <a href="#contact">Contact Me</a>
-      </div>
+      <a href="#" className='toggle-btn' onClick={() => {
+        setMenuOpen(prevState => !prevState)
+      }}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <ul className={`nav-sections ${menuOpen ? 'active' : ''}`}>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Prices</a></li>
+        <li><a href="#contact">Contact Me</a></li>
+      </ul>
     </nav>
   )
 }
