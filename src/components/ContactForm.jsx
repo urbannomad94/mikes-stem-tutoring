689d6b491ac0006b5b5f3711b397cfaead242cc5
Aidan Williams <aidanw4@gmail.com>
2023-01-20 14:00:52 +0700

    add services section

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -7,55 +7,58 @@ export const ContactForm = () => {
     }
     
     return (
-        <form id='contact' onSubmit={handleSubmit}>
-            <div className="form-input">
-                <label htmlFor="name">
-                    Full Name
-                </label>
-                <input
-                    id="name"
-                    type="text" 
-                    name="name"
-                />
-                <ValidationError 
-                    prefix="Name" 
-                    field="name"
-                    errors={state.errors}
-                />
-            </div>
-            <div className="form-input">
-                <label htmlFor="email">
-                    Email Address
-                </label>
-                <input
-                    id="email"
-                    type="email" 
-                    name="email"
-                />
-                <ValidationError 
-                    prefix="Email" 
-                    field="email"
-                    errors={state.errors}
-                />
-            </div>
-            <div className="form-input">
-                <label htmlFor="message">
-                    Enter your message
-                </label>
-                <textarea
-                    id="message"
-                    name="message"
-                />
-                <ValidationError 
-                    prefix="Message" 
-                    field="message"
-                    errors={state.errors}
-                />
-            </div> 
-            <button type="submit" className='btn' disabled={state.submitting}>
-                Submit
-            </button>
-            <span>Reach out to me and I will get back to you as soon as possible.</span>
-        </form>
+        <section>
+            <h1 className='section-heading'>Contact Me</h1>
+            <form id='contact' onSubmit={handleSubmit}>
+                <div className="form-input">
+                    <label htmlFor="name">
+                        Full Name
+                    </label>
+                    <input
+                        id="name"
+                        type="text" 
+                        name="name"
+                    />
+                    <ValidationError 
+                        prefix="Name" 
+                        field="name"
+                        errors={state.errors}
+                    />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="email">
+                        Email Address
+                    </label>
+                    <input
+                        id="email"
+                        type="email" 
+                        name="email"
+                    />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="email"
+                        errors={state.errors}
+                    />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="message">
+                        Enter your message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                    />
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                </div> 
+                <button type="submit" className='btn' disabled={state.submitting}>
+                    Submit
+                </button>
+                <span>Reach out to me and I will get back to you as soon as possible.</span>
+            </form>
+        </section>
     );
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,20 @@ export const Services = () => {
   return (
     <section id='services'>
       <h1 className='section-heading'>Services</h1>
+      <div className="services-container">
+        <div className="service">
+          <h2>Service 1</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus, ea harum architecto laborum maxime blanditiis illum excepturi consequuntur veritatis, ad deleniti dolorem, odio voluptates accusantium cum totam earum vero.</p>
+        </div>
+        <div className="service">
+          <h2>Service 2</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus, ea harum architecto laborum maxime blanditiis illum excepturi consequuntur veritatis, ad deleniti dolorem, odio voluptates accusantium cum totam earum vero.</p>
+        </div>
+        <div className="service">
+          <h2>Service 3</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus, ea harum architecto laborum maxime blanditiis illum excepturi consequuntur veritatis, ad deleniti dolorem, odio voluptates accusantium cum totam earum vero.</p>
+        </div>
+      </div>
     </section>
   )
 }
